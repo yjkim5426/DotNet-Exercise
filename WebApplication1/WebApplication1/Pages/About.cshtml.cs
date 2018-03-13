30b5ebd972ab0739eb5e9371d7c90fca94604b41
YJ <yjkim5426@gmail.com>
2018-03-14 11:01:30 +1300

    init
    Quickstart: Use Visual Studio to create your first ASP.NET Core web app
    
    https://docs.microsoft.com/en-us/visualstudio/ide/quickstart-aspnet-core

---
+++ WebApplication1/WebApplication1/Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace WebApplication1.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            //Message = "Your application description page.";
+
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Your directory is {0}.", directory);
+        }
+    }
+}
---
+++ WebApplication_test/WebApplication_test/Pages/About.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace WebApplication_test.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            //Message = "Your application description page.";
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Your directory is {0}.", directory);
+        }
+    }
+}
